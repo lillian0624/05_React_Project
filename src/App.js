43083dcbd62fac274617daebc1b289c1a8199578
Lillian Yang <nopeyangli@163.com>
2022-07-03 11:04:41 +1000

    state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-/*类组件
-import React from "react";
-
-class App extends React.Component {
-  render() {
-    return <div>I am a module</div>;
-  }
-}
-*/
-import Logs from "./Logs/Logs";
+import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const result = useState(4);
+  // let counter = result[0];
+  // let setCounter =result[1];
+
+  const [counter, setCounter]=result;
+ 
+  const addHandler = () => {
+
+    setCounter(counter+1);
+  
+  };
+
+  const lessHandler = () => { 
+    // alert('-1');
+   setCounter(counter-1);
+    
+  };
+
   return (
-    <div>
-      <Logs />
+    <div className={"app"}>
+      <h1>{counter}</h1>
+      <button onClick={lessHandler}>-</button>
+      <button onClick={addHandler}>+</button>
     </div>
   );
 };
