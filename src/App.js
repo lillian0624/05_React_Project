00a3ed2be8c8b66aa3d908829bb0aef060f6fa51
Lillian Yang <nopeyangli@163.com>
2022-07-03 12:47:18 +1000

    state

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
-  const result = useState(4);
-  // let counter = result[0];
-  // let setCounter =result[1];
+  const [counter, setCounter] = useState(4);
+  const [user, setUser] = useState({ name: "Lillian", age: 999 });
 
-  const [counter, setCounter]=result;
- 
   const addHandler = () => {
-
-    setCounter(counter+1);
-  
+    setTimeout(() => setCounter((prevCounter) => prevCounter + 1), 1000);
   };
 
-  const lessHandler = () => { 
+  const lessHandler = () => {
     // alert('-1');
-   setCounter(counter-1);
-    
+    setCounter(counter - 1);
+  };
+
+  const upDateUserHandler = () => {
+    // const newUser = Object.assign({} ,user);
+    // newUser.name = 'xiaoyanng';
+    // setUser(newUser);
+    setUser({ ...user, name: "xx" });
   };
 
   return (
     <div className={"app"}>
-      <h1>{counter}</h1>
-      <button onClick={lessHandler}>-</button>
+      <h1>
+        {counter}--{user.name}--{user.age}
+      </h1>
+      {/* <button onClick={lessHandler}>-</button> */}
       <button onClick={addHandler}>+</button>
+      <button onClick={upDateUserHandler}>3</button>
     </div>
   );
 };
